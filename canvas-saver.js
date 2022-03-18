174e8120e801a8679ebf341f2d79e5b2e709f9d9
sir <sir@nabistudios.com>
2022-03-18 07:59:46 +0100

    Better logging with timestamp, make sure process exits on completion

--- canvas-saver.js
+++ canvas-saver.js
@@ -2,13 +2,23 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const config = require('./config.json');
 
+function dateNow() {
+	const date = new Date();
+	return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "." + date.getMilliseconds();
+}
+
+function logMessage(msg) {
+	console.log(dateNow() + " | " + msg);
+}
+
 const args = process.argv.slice(2);
 if (typeof args[0] === 'undefined') {
-	console.log("Usage: `node canvas-saver.js username|userid`");
-	return;
+	logMessage("Usage error: `node canvas-saver.js username|userid`");
+	process.exit(1);
 }
 
 (async () => {
+	logMessage("Fetching player preview for " + args[0]);
 	const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-web-security', '--enable-features=NetworkService'] });
 	const page = await browser.newPage();
 	await page.goto('http://localhost/playerpreview.php?u=' + args[0]);
@@ -25,4 +35,5 @@ if (typeof args[0] === 'undefined') {
 	fs.writeFileSync(config.path + userid + ".png", image);
 
 	await browser.close();
+	process.exit(0);
 })();
